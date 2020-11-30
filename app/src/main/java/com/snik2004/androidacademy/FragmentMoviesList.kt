06a33c19876f4e11fa51df4c43fcb22d78e4d37c
garbarnp <snik2004@gmail.com>
2020-11-30 17:22:11 +0600

    homework3 prefinal

---
+++ app/src/main/java/com/snik2004/androidacademy/FragmentMoviesList.kt
@@ -0,0 +1,63 @@
+package com.snik2004.androidacademy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.snik2004.androidacademy.dummy.DummyContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class FragmentMoviesList : Fragment() {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+        view.findViewById<ImageView>(R.id.card_movie)
+            .setOnClickListener {
+                activity?.let {
+                    it.supportFragmentManager.beginTransaction()
+                        .addToBackStack(null)
+                        .replace(R.id.frame_container, FragmentMoviesDetails())
+                        .commit()
+                }
+            }
+
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            FragmentMoviesList().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
--- app/src/main/java/com/snik2004/androidacademy/MainActivity.kt
+++ app/src/main/java/com/snik2004/androidacademy/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.movie_activity_detail)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.frame_container, FragmentMoviesList())
+                .commit()
+        }
     }
 }
