f2add9ef330a256922652de2f2525dada73ac680
garbarnp <snik2004@gmail.com>
2020-12-07 15:18:17 +0600

    homework4 list begin

---
+++ app/src/main/java/com/snik2004/androidacademy/Movie.kt
@@ -0,0 +1,12 @@
+package com.snik2004.androidacademy
+
+class Movie  {
+    var nameMovie = null
+    var duration = null
+    var rating = 0
+    var genre =null
+    var pg = null
+    var reviews = null
+    var linkPoster = null
+
+}
