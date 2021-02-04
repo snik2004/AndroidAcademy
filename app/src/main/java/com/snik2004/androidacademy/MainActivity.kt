b139a7ec02aec6a9a3f8e6530b11889d55e26d31
garbarnp <snik2004@gmail.com>
2021-02-04 10:59:07 +0600

    homework view code review 2 edits

--- app/src/main/java/com/snik2004/androidacademy/MainActivity.kt
+++ app/src/main/java/com/snik2004/androidacademy/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.movie_activity_detail)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .add(R.id.frame_container, FragmentMoviesList())
+//                .commit()
+//        }
+
     }
 }
